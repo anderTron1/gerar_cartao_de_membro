e20fd04950dc5cebbd6c8be40b2c21046dd288d9
André Luiz <andre-luizpiresguimaraes@outlook.com>
2022-03-14 07:48:54 -0400

    new update

--- cartão.py
+++ cartão.py
@@ -31,20 +31,41 @@ def editImage(imgToEdit, nameNewImage):
     cv2.imshow("janela",img)
     
     cv2.imwrite(nameNewImage,img)
-    cv2.waitKey(0)
+    #cv2.waitKey(0)
+    
+def editImageFundo(imgToEdit, nameNewImage):
+    img = cv2.imread(imgToEdit)
+    
+    cv2.putText(img,"RAMILTON RIBEIRO GUIMARAES",(180,110),cv2.FONT_HERSHEY_TRIPLEX,1,0)
+    cv2.putText(img,"JUCELIA PEREIRA PIRES",(180,190),cv2.FONT_HERSHEY_TRIPLEX,1,0)
+    cv2.putText(img,"BRASILEIRO",(180,290),cv2.FONT_HERSHEY_TRIPLEX,1,0)
+    cv2.putText(img,"MACULINO",(720,290),cv2.FONT_HERSHEY_TRIPLEX,1,0)
+    cv2.putText(img,"15454813",(180,390),cv2.FONT_HERSHEY_TRIPLEX,1,0)
+    cv2.putText(img,"703.455.081-65",(620,390),cv2.FONT_HERSHEY_TRIPLEX,1,0)
+    
+    
+    cv2.imshow("janela",img)
+    
+    cv2.imwrite(nameNewImage,img)
+    #cv2.waitKey(0)
 
 
 
 from reportlab.pdfgen import canvas
 from PIL import Image
-from resizeimage import resizeimage
 
 
 editImage('fundo_frente.bmp', 'fundo_frente.jpg')
-editImage('fundo_fundo.bmp', 'fundo_fundo.jpg')
+editImageFundo('fundo_fundo.bmp', 'fundo_fundo.jpg')
 
-def trataImage(nameImg):
+def trataImage(nameImg, insertImage=True):
     img = Image.open(nameImg)
+    imgPNG = Image.open('image.png')
+
+    if insertImage:
+        imgPNG = imgPNG.resize((220, 280), Image.ANTIALIAS)
+        img.paste(imgPNG, (40,410))
+    
     img = img.rotate(90, expand=True)  
     img = img.resize((733, 1068), Image.ANTIALIAS)
     
@@ -52,14 +73,18 @@ def trataImage(nameImg):
     
     img.save(nameImg)
     img.close()
+    imgPNG.close()
+    
     
 trataImage("fundo_frente.jpg")
-trataImage("fundo_fundo.jpg")
+trataImage("fundo_fundo.jpg", False)
 
-image = os.path.join("fundo_frente.jpg")
+#image = os.path.join("fundo_frente.jpg")
+#image2 = os.path.join("fundo_frente.jpg")
 
 c = canvas.Canvas("arquivo.pdf")
-c.drawImage(image, 1,600, width=151, height=240)
+c.drawImage("fundo_frente.jpg", 1,600, width=151, height=240)
+c.showPage()
+c.drawImage("fundo_fundo.jpg", 1,600, width=151, height=240)
 
 c.save()
-
