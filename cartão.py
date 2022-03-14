9a0fcc6c92e9ad07efbcf1e7b4085b273bbd108f
Andre Luiz <92170539+anderTron1@users.noreply.github.com>
2022-03-14 17:56:17 -0300

    Update cartão.py

--- cartão.py
+++ cartão.py
@@ -6,7 +6,6 @@ Created on Fri Mar 11 14:38:13 2022
 
 """
 
-
 print('test2e')
 import cv2
 import numpy as np
@@ -16,19 +15,16 @@ def editImage(imgToEdit, nameNewImage):
     img = cv2.imread(imgToEdit)
     imgPNG = cv2.imread('image.png')
     
-    cv2.putText(img,"5154",(870,260),cv2.FONT_HERSHEY_TRIPLEX,1,0)
-    cv2.putText(img,"ANDRE LUIZ PIRES GUIMARAES",(100,360),cv2.FONT_HERSHEY_TRIPLEX,1,0)
-    cv2.putText(img,"DIACONO",(850,360),cv2.FONT_HERSHEY_TRIPLEX,1,0)
-    cv2.putText(img,"TEMPLO SEDE",(440,450),cv2.FONT_HERSHEY_TRIPLEX,1,0)
-    cv2.putText(img,"MT",(880,450),cv2.FONT_HERSHEY_TRIPLEX,1,0)
-    cv2.putText(img,"SOLTEIRO",(460,550),cv2.FONT_HERSHEY_TRIPLEX,1,0)
-    cv2.putText(img,"15/07/1996",(805,550),cv2.FONT_HERSHEY_TRIPLEX,1,0)
-    cv2.putText(img,"12/03/2022",(350,640),cv2.FONT_HERSHEY_TRIPLEX,1,0)
-    cv2.putText(img,"12/03/2027",(730,640),cv2.FONT_HERSHEY_TRIPLEX,1,0)
-    #cv2.addWeighted(img, 0.5, imgPNG, 0.8, 0.0)
+    cv2.putText(img,"ANDRE LUIZ PIRES GUIMARAES",(100,330),cv2.FONT_HERSHEY_TRIPLEX,1,0)
+    cv2.putText(img,"DIACONO",(120,420),cv2.FONT_HERSHEY_TRIPLEX,1,0)
+    cv2.putText(img,"15/07/1996",(490,420),cv2.FONT_HERSHEY_TRIPLEX,1,0)
+    cv2.putText(img,"14/03/2022",(120,510),cv2.FONT_HERSHEY_TRIPLEX,1,0)
+    cv2.putText(img,"12/03/2027",(440,510),cv2.FONT_HERSHEY_TRIPLEX,1,0)
+    cv2.putText(img,"15648154-15",(120,610),cv2.FONT_HERSHEY_TRIPLEX,1,0)
+    cv2.putText(img,"703.455.081-65",(420,610),cv2.FONT_HERSHEY_TRIPLEX,1,0)
     
     
-    cv2.imshow("janela",img)
+    #cv2.imshow("janela",img)
     
     cv2.imwrite(nameNewImage,img)
     #cv2.waitKey(0)
@@ -44,28 +40,85 @@ def editImageFundo(imgToEdit, nameNewImage):
     cv2.putText(img,"703.455.081-65",(620,390),cv2.FONT_HERSHEY_TRIPLEX,1,0)
     
     
-    cv2.imshow("janela",img)
+    #cv2.imshow("janela",img)
     
     cv2.imwrite(nameNewImage,img)
     #cv2.waitKey(0)
 
-
-
 from reportlab.pdfgen import canvas
-from PIL import Image
+from PIL import Image, ImageDraw
+import numpy as np
 
 
-editImage('fundo_frente.bmp', 'fundo_frente.jpg')
-editImageFundo('fundo_fundo.bmp', 'fundo_fundo.jpg')
+def round_corner(image, offset, width, height, filled_pixel):
+
+    im = Image.open(image).convert('RGBA').resize((width - offset, height - offset, Image.ANTIALIAS))
+    im_base = Image.new('RGBA', (width, height), (255, 255, 255, 255))
+    
+    #im_base =ImageDraw.Draw(im)
+    im_base.paste(im, (offset / 2, offset / 2))
+    im = im_base
+
+    im_new = Image.new('L', (width, height))
+
+    half_w, half_h = width / 2, height / 2
+
+    for x in range(width):
+        for y in range(height):
+            if (x - half_w) * (x - half_w) + (y - half_h) * (y - half_h) <= half_w * half_h:
+                pixel = im.getpixel((x, y))
+            else:
+                pixel = filled_pixel
+            im_new.putpixel((x, y), pixel)
+
+    return im_new
+
+
+editImage('cartao_frente.jpeg', 'fundo_frente.jpg')
+editImageFundo('cartao_fundo.jpeg', 'fundo_fundo.jpg')
 
 def trataImage(nameImg, insertImage=True):
     img = Image.open(nameImg)
-    imgPNG = Image.open('image.png')
+    imgPNG = Image.open('image.jpg')#.convert('L')
+    #method = Image.EXTENT
+    #    data = (0, 0, 100, 100)
 
     if insertImage:
-        imgPNG = imgPNG.resize((220, 280), Image.ANTIALIAS)
-        img.paste(imgPNG, (40,410))
-    
+        #imgPNG = imgPNG.resize((209, 259))
+        
+        #imgPNG = round_corner('image.png', 0, 210, 260, (0, 0, 0, 0))
+        #icon_base = Image.new('RGBA', imgPNGA.size, (255, 255, 255, 0))
+        
+        
+        
+        #result = Image.new('L', (209, 259), 0)
+        #draw = ImageDraw.Draw(result)
+        #draw.rounded_rectangle(((0, 0), (210, 260)), 20, fill=255,outline = "white")
+
+        height,width = imgPNG.size
+        lum_img = Image.new('L', (height,width),0)
+          
+        draw = ImageDraw.Draw(lum_img)
+        draw.rounded_rectangle(((0, 0), (height, width)), 95, fill="white",outline = "white")
+        
+        img_arr =np.array(imgPNG)
+        lum_img_arr =np.array(lum_img)
+        #display(Image.fromarray(lum_img_arr))
+        final_img_arr = np.dstack((img_arr, lum_img_arr))
+        #display(Image.fromarray(final_img_arr))
+
+        img_final= Image.fromarray(final_img_arr)
+        
+        img_final.thumbnail((214,264))
+        
+        img_final.save('out.png')
+        
+        img_ = Image.open('out.png')
+        
+        img.paste(img_, (735,365))
+        
+        
+        
     img = img.rotate(90, expand=True)  
     img = img.resize((733, 1068), Image.ANTIALIAS)
     
@@ -73,7 +126,6 @@ def trataImage(nameImg, insertImage=True):
     
     img.save(nameImg)
     img.close()
-    imgPNG.close()
     
     
 trataImage("fundo_frente.jpg")
